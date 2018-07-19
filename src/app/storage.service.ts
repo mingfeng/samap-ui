a20fbe618df4a50609be129ccfe16d0364dc2c5b
Mingfeng Yu <mfdevworld@gmail.com>
2018-07-19 19:38:18 +0300

    Add a simple storage service

---
+++ src/app/storage.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+import { Settings } from './interfaces/settings';
+
+
+const DEFAULT_DISTANCE = 1000;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+  private _settings: Settings;
+
+  constructor() { }
+
+  get settings(): Settings {
+    if (!this._settings) {
+      this._settings = {
+        distance: parseInt(localStorage.getItem('settings-distance'), 10) || DEFAULT_DISTANCE
+      };
+    }
+
+    return this._settings;
+  }
+
+  set settings(settings: Settings) {
+    this._settings = settings;
+    localStorage.setItem('settings-distance', settings.distance.toString());
+  }
+}
